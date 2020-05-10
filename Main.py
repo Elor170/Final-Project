afd202241986d9906aaa59586af07626d50c1c98
Elor170 <elor170@gmail.com>
2020-05-10 10:25:17 +0300

    Add- cut the part of write: (1) Append (2) Separate

--- Main.py
+++ Main.py
@@ -1,64 +1,49 @@
 import cv2
-import requests
 import Constants
-import numpy as np
 from Scan_board import scan_board
 from Detect_board import detect_board
 
 # video source
-path = 'Resources/Lesson 6__.avi'
+path = 'Resources/Lesson_1.mp4'
 cap = cv2.VideoCapture(path)
-# cap = cv2.VideoCapture(0)
 
+case = Constants.SEPARATE_CASE
 is_first = True
-is_writing = False
+is_writing = True
+is_scanned = True
 time_no_write = 0
-is_scanned = False
-frames_count = 0
+counter = 1
 success, original_frame = cap.read()
 
 while success:
-    original_frame = original_frame[0:900, :, :]
-    original_frame = cv2.resize(original_frame, (1000, Constants.HEIGHT_FRAME))
-    frame = original_frame
-    imgWarped, coordinates = detect_board(frame)
-    if imgWarped is 0:
+    original_frame = cv2.resize(original_frame, (Constants.HEIGHT_FRAME, Constants.WIDTH_FRAME))
+    processed_frame, is_writing = detect_board(original_frame)
+    # while write
+    if is_writing:
         time_no_write = 0
-        is_writing = True
         is_scanned = False
-        cv2.circle(frame, (20, 20), 10, (0, 0, 255), -1)
+        cv2.circle(processed_frame, (20, 20), 10, (0, 0, 255), -1)
+    # while not write
     else:
         time_no_write += 1
-        frame = imgWarped
-        cv2.circle(frame, (20, 20), 10, (255, 255, 0), -1)
+        cv2.circle(processed_frame, (20, 20), 10, (255, 255, 0), -1)
+        # first frame
         if is_first:
-            x = coordinates[0][0][0]
-            y = coordinates[0][0][1]
-            w = coordinates[3][0][0] - coordinates[0][0][0]
-            h = coordinates[3][0][1] - coordinates[0][0][1]
-            # print(x, y, w, h)
-            # cv2.imshow("board", original_frame[y + 15: y + h - 15, x + 15: x + w - 15])
             is_first = False
-            b_frame = original_frame[y + 15: y + h - 15, x + 15: x + w - 15]
-            # b_frame = imgWarped[y + 5: y + h - 5, x + 5: x + w - 5]
-        elif time_no_write > 5 and not is_scanned:
-            is_writing = False
-            a_frame = original_frame[y + 15: y + h - 15, x + 15: x + w - 15]
-            # a_frame = imgWarped[y + 5: y + h - 5, x + 5: x + w - 5]
-            # cv2.imshow("After", a_frame)
-            # cv2.imshow("Before", b_frame)
-            scan_board(b_frame, a_frame, frames_count)
-            b_frame = a_frame
+            before_frame = processed_frame
+        # scan the board
+        elif time_no_write > 60 and not is_scanned:
+            after_frame = processed_frame
+            counter, board = scan_board(before_frame, after_frame, counter, case)
+            before_frame = after_frame
             is_scanned = True
-
-    cv2.imshow("Lesson", frame)
-    # cv2.imshow("O_Lesson", original_frame)
-    # if is_first:
-    #     cv2.waitKey(0)
-    #     is_first = False
+    cv2.imshow("Lesson", processed_frame)
     success, original_frame = cap.read()
     if cv2.waitKey(1) & 0xFF == Constants.ESC_KEY_BOARD:
         break
+
+if case is Constants.APPEND_CASE:
+    cv2.imwrite("Output/Board.jpg", board)
 
 cap.release()
 cv2.destroyAllWindows()
--- Scan_board.py
+++ Scan_board.py
@@ -2,60 +2,74 @@ import cv2
 from skimage.measure import compare_ssim
 import imutils
 import Constants
-import argparse
 import numpy as np
 
+board = np.full((Constants.HEIGHT_FRAME, Constants.WIDTH_FRAME, 3), 125, dtype=np.uint8)
 
-def scan_board(b_img, a_img, num):
-    gray_b = cv2.cvtColor(b_img, cv2.COLOR_RGB2GRAY)
-    gray_a = cv2.cvtColor(a_img, cv2.COLOR_RGB2GRAY)
-    # gray_b = cv2.GaussianBlur(gray_b, (5, 5), 2)
-    # gray_a = cv2.GaussianBlur(gray_a, (5, 5), 2)
+
+def scan_board(b_img, a_img, counter, case):
+    b_img_blur = cv2.GaussianBlur(b_img, (5, 5), 50, 50, 50)
+    a_img_blur = cv2.GaussianBlur(a_img, (5, 5), 50, 50, 50)
+    gray_b = cv2.cvtColor(b_img_blur, cv2.COLOR_RGB2GRAY)
+    gray_a = cv2.cvtColor(a_img_blur, cv2.COLOR_RGB2GRAY)
+
     # compute the Structural Similarity Index (SSIM) between the two
     # images, ensuring that the difference image is returned
     (score, diff) = compare_ssim(gray_b, gray_a, full=True)
     diff = (diff * 255).astype("uint8")
     print("SSIM: {}".format(score))
-    if score < 0.97:
+    if score < 0.99:
+
         # threshold the difference image, followed by finding contours to
         # obtain the regions of the two input images that differ
-        thresh = cv2.threshold(diff, 0, 255,
-                               cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
+        thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
 
         x_min = Constants.WIDTH_FRAME
         y_min = Constants.HEIGHT_FRAME
         x_max = 0
         y_max = 0
+
         # loop over the contours
         for c in cnts:
             # compute the bounding box of the contour and then draw the
             # bounding box on both input images to represent where the two
             # images differ
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(gray_b, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.rectangle(gray_a, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            # if h > 30 and w > 30:
-            #     if x < x_min:
-            #         x_min = x
-            #     if y < y_min:
-            #         y_min = y
-            #     if x > x_max:
-            #         x_max = y
-            #     if y > y_max:
-            #         y_max = y
+            area = cv2.contourArea(c)
+            if area > 200:
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(gray_b, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.rectangle(gray_a, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
+                if x < x_min:
+                    x_min = x
+                if y < y_min:
+                    y_min = y
+                if x + w > x_max:
+                    x_max = x + w
+                if y + h > y_max:
+                    y_max = y + h
         # show the output images
-        cv2.imshow("Original", gray_b)
-        cv2.imshow("Modified", gray_a)
-        cv2.imshow("Diff", diff)
-        cv2.imshow("Thresh", thresh)
-        # (x, y, w, h) = cv2.boundingRect(thresh)
-        # write the output
-        # print(x, y, w, h)
-        # output = a_img[x:x+w, y:y+h, :]
-        # cv2.imshow("Output", output)
-        cv2.waitKey(0)
+        # cv2.imshow("Original", gray_b)
+        # cv2.imshow("Modified", gray_a)
+        # cv2.imshow("Diff", diff)
+        # cv2.imshow("Thresh", thresh)
+        cv2.imshow("Lesson", gray_a)
+        cv2.imshow("Lesson_b", gray_b)
+
+        cut = a_img[y_min:y_max, x_min:x_max]
+
+        if case is Constants.SEPARATE_CASE:
+            cv2.imshow("Cut", cut)
+            cv2.imwrite("Output/Board%d.jpg" % counter, cut)
+            counter += 1
+
+        if case is Constants.APPEND_CASE:
+            board[y_min:y_max, x_min:x_max] = cut
+            cv2.imshow("Added to board", board)
+
+        cv2.waitKey(1000)
         cv2.destroyAllWindows()
+    return counter, board
+
